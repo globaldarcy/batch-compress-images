a00c472716037e2b18b5442404ccf086924152ac
Darcy <289904316@qq.com>
2020-06-30 15:58:11 +0800

    update jpeg parameters

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # 批量压缩图像/batch-compress-images
 
+Jpeg 图片压缩参数进行调整，提高了压缩图片的速度。
+
 > 用于批量压缩图像的工具，类似于在线压缩图像的一些工具。 它更方便，更快捷，适合开发人员使用。
 
 > A tool for compressing images in batches, similar to some tools for compressing images online. It is more convenient and faster, suitable for developers.
 
-支持图片格式: jpg, png, gif, svg
+支持图片格式: jpg, png, svg
 
-Support image format: jpg, png, gif, svg
+Support image format: jpg, png, svg
 
 ## 安装/Install
 克隆到本地
@@ -23,9 +25,9 @@ $ npm install
 
 Put the image folder you want to compress in the current project's directory, and the folder and image are named in English. Support for folder level relationships.
 
-demo 5.9 MB, 压缩后为2 MB
+demo 4.6MB, 压缩后为 749KB
 
-demo 5.9 MB, 2 MB after compression
+demo 4.6MB, 749KB after compression
 
 ```
 $ npm start
--- main.js
+++ main.js
@@ -1,39 +1,38 @@
 const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
+const imageminOptipng = require('imagemin-optipng');
 const imageminGifsicle = require('imagemin-gifsicle');
 const imageminSvgo = require('imagemin-svgo');
-const glob = require('glob');
 const path = require('path');
+const globby = require('globby');
 
-glob('**/*.{jpeg,jpg,png,gif,svg,JPEG,JPG,PNG,GIF,SVG}', function (er, files) {
-    files.filter(file => {
-        if (file.indexOf('node_modules') === -1 && file.indexOf('build') === -1) {
-            imagemin([file], 'build/' + path.dirname(file), {
-                plugins: [
-                    imageminMozjpeg({
-                        progressive: true
-                    }),
-                    imageminPngquant({
-                        quality: [0.6, 0.7]
-                    }),
-                    imageminGifsicle({ interlaced: true }),
-                    imageminSvgo({
-                        plugins: [
-                            {
-                                removeViewBox: true,
-                                removeTitle: true,
-                                removeDesc: true,
-                            }
-                        ]
-                    })
-                ]
-            }).then(files => {
-                console.log('压缩成功/SuccessfulCompression: ' + files[0].path);
-                // => [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
-            });
-        }
+(async () => {
+    const files = await globby('**/*.{jpeg,jpg,png,gif,svg,JPEG,JPG,PNG,GIF,SVG}', {
+        ignore: ['node_modules', 'build']
     });
-    console.log(er);
-});
+
+    files.filter(file => {
+        imagemin([file], {
+            destination: 'build/' + path.dirname(file),
+            plugins: [
+                imageminGifsicle(),
+                imageminMozjpeg({
+                    quality: 75,
+                    smooth: 0,
+                    quantTable: 3,
+                }),
+                imageminPngquant({
+                    quality: [0.7, 0.8] // 参数可调整，这个压缩比大，图片有损失。
+                }),
+                // imageminOptipng(), // 这个压缩比小，但是图片清晰度高。
+                imageminSvgo({
+                    plugins: [{ removeViewBox: true, }]
+                })
+            ]
+        }).then(files => {
+            console.log('压缩成功 / Successful Compression: ' + files[0].sourcePath);
+        });
+    });
+})();
 // RUN =======> [npm start] or [node main.js]
